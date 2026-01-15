4e11ecc6dc631be808d46d6cb22417165c65dc04
yasmineh-d <yassminehadad75@gmail.com>
2026-01-15 15:38:58 +0000

    tasks in progress

--- reading-app/routes/web.php
+++ reading-app/routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\UserController;
-
-Route::resource('books', BookController::class);
-Route::get('/categories', [CategoryController::class, 'index']);
-
-Route::resource('users', UserController::class);
-
-
 Route::get('/', function () {
     return view('welcome');
 });
