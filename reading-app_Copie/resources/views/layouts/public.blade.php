b56363bdc7b9d03c4f9038abc461022dc2941c83
yasmineh-d <yassminehadad75@gmail.com>
2026-02-12 13:21:14 +0000

    la partie gares is done

--- reading-app_Copie/resources/views/layouts/public.blade.php
+++ reading-app_Copie/resources/views/layouts/public.blade.php
@@ -48,10 +48,12 @@
                                     </button>
                                 </div>
                                 <div id="user-dropdown" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu">
-                                    <a href="{{ route('admin.books.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Dashboard</a>
+                                    @if(Auth::user()->isAdmin())
+                                        <a href="{{ route('admin.books.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Dashboard Admin</a>
+                                    @endif
                                     <form method="POST" action="{{ route('logout') }}">
                                         @csrf
-                                        <button type="submit" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</button>
+                                        <button type="submit" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Logout</button>
                                     </form>
                                 </div>
                             </div>
